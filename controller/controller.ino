70306327f0f62a1711e6e3df64e3cc1cccfaaa99
Khaled <khaledsoultan63@gmail.com>
2019-07-21 18:25:47 +0100

    Implemented bi-directional control of the right wheel

--- controller/controller.ino
+++ controller/controller.ino
@@ -11,6 +11,8 @@ std_msgs::Int16 right;
  * =========Right Motor==========
   -Motor yellow --> Arduino D2 (encoder phase A)
   -Motor green  --> Arduino D3 (encoder phase B)
+  -H-bridge IN1 --> Arduino D10
+  -H-bridge IN2 --> Arduino D9
 
   ==========Left Motor===========
   -Motor yellow --> Arduino D19 (encoder phase A)
@@ -21,16 +23,15 @@ std_msgs::Int16 right;
   -Motor white --> OUT2 H-bridge
   -Motor blue  --> 3.3V (encoder power)
   -Motor black --> GND (encoder power)
-  -H-bridge IN1 --> Arduino D10
-  -H-bridge IN2 --> Arduino D9*/
+  */
 
 const byte R_encoderPinA = 2;
 const byte R_encoderPinB = 3;
 const byte L_encoderPinA = 18;
 const byte L_encoderPinB = 19;
 
-const byte r_motor = 9;
-const byte l_motor = 10;
+const byte r_motor_fwd = 9;
+const byte r_motor_bck = 10;
 
 volatile long R_pulse_count;
 volatile long L_pulse_count;
@@ -72,15 +73,23 @@ void L_encoderB_ISR()
 }
 
 //======================ROS===========================
-void l_messageCb(const std_msgs::Float32& l_cmd){
-  analogWrite(l_motor, int(l_cmd.data));
-}
+//void l_messageCb(const std_msgs::Float32& l_cmd){
+//  analogWrite(l_motor, int(l_cmd.data));
+//}
 
 void r_messageCb(const std_msgs::Float32& r_cmd){
-  analogWrite(r_motor, int(r_cmd.data));
+  if(r_cmd.data > 0){
+    analogWrite(r_motor_fwd, abs(int(r_cmd.data)));
+    analogWrite(r_motor_bck, 0);
+  }
+    
+  else{  
+    analogWrite(r_motor_bck, abs(int(r_cmd.data)));
+    analogWrite(r_motor_fwd, 0);
+  } 
 }
 
-ros::Subscriber<std_msgs::Float32> l_sub("l_motor_cmd", &l_messageCb );
+//ros::Subscriber<std_msgs::Float32> l_sub("l_motor_cmd", &l_messageCb );
 ros::Subscriber<std_msgs::Float32> r_sub("r_motor_cmd", &r_messageCb );
 
 ros::Publisher lwheel("lwheel", &left);
@@ -92,7 +101,7 @@ void setup()
   nh.initNode();
   nh.advertise(lwheel);
   nh.advertise(rwheel);
-  nh.subscribe(l_sub);
+//  nh.subscribe(l_sub);
   nh.subscribe(r_sub); 
 
   pinMode(R_encoderPinA, INPUT_PULLUP);
@@ -100,8 +109,9 @@ void setup()
   pinMode(L_encoderPinB, INPUT_PULLUP);
   pinMode(L_encoderPinB, INPUT_PULLUP);
 
-  pinMode(r_motor, OUTPUT);
-  pinMode(l_motor, OUTPUT);
+  pinMode(r_motor_fwd, OUTPUT);
+  pinMode(r_motor_bck, OUTPUT);
+//  pinMode(l_motor, OUTPUT);
 
   attachInterrupt(digitalPinToInterrupt(R_encoderPinA), R_encoderA_ISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(R_encoderPinB), R_encoderB_ISR, CHANGE);
